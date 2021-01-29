b268d834c9966993787b302165bc18f92a69025a
Seong Joon Oh <7447092+coallaoh@users.noreply.github.com>
2021-01-29 16:45:49 +0900

    Update setup_zsh.sh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -3,3 +3,5 @@ chsh -s $(which zsh)
 wget https://raw.githubusercontent.com/coallaoh/rc/master/.zshrc
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mv .zshrc.pre-oh-my-zsh .zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
